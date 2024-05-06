f1bf38ee6d16f097412377ffc811f8c879e12495
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-05-06 20:56:45 +0500

    Подготовлен шаблон

---
+++ FurnitureStore/FurnitureStore/FurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureStoreApp.swift
+//  FurnitureStore
+//
+//  Created by Dmitriy Starozhilov on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
